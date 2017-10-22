f4da3db74db453c489ae2fcfeb253798cbcea690
liubo <liu@liubo.com>
2017-10-22 14:12:59 +0800

    add requirement and sql file

---
+++ movie.sql
@@ -0,0 +1,400 @@
+-- MySQL dump 10.13  Distrib 5.7.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: movie
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `pwd` varchar(128) DEFAULT NULL,
+  `is_super` smallint(6) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `role_id` (`role_id`),
+  KEY `ix_admin_addtime` (`addtime`),
+  CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES (1,'imoocmovie','pbkdf2:sha256:50000$pEAYwHIz$ab6b370999fa1deed9cd5cc078d684b7e15b796e275677f738c7011950481863',1,1,'2017-10-06 07:38:24'),(2,'imoocmovie11','pbkdf2:sha256:50000$zlALIg7F$eae08d5221a09ff794262f8b62ae8ef031a7389fa702123c381d5298db846033',0,1,'2017-10-06 15:41:09'),(3,'abo','pbkdf2:sha256:50000$8QE9DYTe$248d8c188d32c62ce96935b11dcb92a71b00f0b27150bedaef10075c493781f3',1,3,'2017-10-15 22:28:38');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `adminlog`
+--
+
+DROP TABLE IF EXISTS `adminlog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `adminlog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_id` int(11) DEFAULT NULL,
+  `ip` varchar(64) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `admin_id` (`admin_id`),
+  KEY `ix_adminlog_addtime` (`addtime`),
+  CONSTRAINT `adminlog_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adminlog`
+--
+
+LOCK TABLES `adminlog` WRITE;
+/*!40000 ALTER TABLE `adminlog` DISABLE KEYS */;
+INSERT INTO `adminlog` VALUES (1,1,'127.0.0.1','2017-10-15 20:37:45'),(2,1,'127.0.0.1','2017-10-15 20:37:53'),(3,3,'127.0.0.1','2017-10-15 23:21:52'),(4,3,'127.0.0.1','2017-10-16 23:46:45'),(5,3,'127.0.0.1','2017-10-16 23:52:17'),(6,3,'127.0.0.1','2017-10-17 21:17:06'),(7,3,'127.0.0.1','2017-10-19 21:40:27');
+/*!40000 ALTER TABLE `adminlog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auth`
+--
+
+DROP TABLE IF EXISTS `auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `auth` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `url` varchar(256) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `url` (`url`),
+  KEY `ix_auth_addtime` (`addtime`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth`
+--
+
+LOCK TABLES `auth` WRITE;
+/*!40000 ALTER TABLE `auth` DISABLE KEYS */;
+INSERT INTO `auth` VALUES (1,'添加标签','/admin/tag/add/','2017-10-15 20:58:10'),(2,'编辑标签','/admin/tag/edit/<int:id>/','2017-10-15 20:59:36'),(3,'标签列表','/admin/tag/list/<int:page>/','2017-10-15 21:00:09');
+/*!40000 ALTER TABLE `auth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text,
+  `movie_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `movie_id` (`movie_id`),
+  KEY `user_id` (`user_id`),
+  KEY `ix_comment_addtime` (`addtime`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movie`
+--
+
+DROP TABLE IF EXISTS `movie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `movie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) DEFAULT NULL,
+  `url` varchar(256) DEFAULT NULL,
+  `info` text,
+  `logo` varchar(256) DEFAULT NULL,
+  `star` smallint(6) DEFAULT NULL,
+  `playnum` bigint(20) DEFAULT NULL,
+  `commentnum` bigint(20) DEFAULT NULL,
+  `tag_id` int(11) DEFAULT NULL,
+  `area` varchar(64) DEFAULT NULL,
+  `release_time` date DEFAULT NULL,
+  `length` varchar(64) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`),
+  UNIQUE KEY `url` (`url`),
+  UNIQUE KEY `logo` (`logo`),
+  KEY `tag_id` (`tag_id`),
+  KEY `ix_movie_addtime` (`addtime`),
+  CONSTRAINT `movie_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movie`
+--
+
+LOCK TABLES `movie` WRITE;
+/*!40000 ALTER TABLE `movie` DISABLE KEYS */;
+INSERT INTO `movie` VALUES (1,'自行车飞轮保养','201710151618338f8c6f8f7d7b441c827681e34cb4a27e.mp4','六八燃料刘家坳而欧皇\r\n啊lrhoqrn\r\nrg离开家爱华仕人潜入\r\n；爱人高尔\r\n；阿仁洪七公和温柔呢 \r\n啊日记哈克把加热体\r\napworjgarmfgoaijr\r\n爱上任何感情偶尔UN发老KN','201710151618334edb1867ccca402c8cfeaea88a722442.JPG',3,130,0,2,'中国','2017-11-01','22','2017-10-15 16:18:34');
+/*!40000 ALTER TABLE `movie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `moviecol`
+--
+
+DROP TABLE IF EXISTS `moviecol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `moviecol` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `movie_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `movie_id` (`movie_id`),
+  KEY `user_id` (`user_id`),
+  KEY `ix_moviecol_addtime` (`addtime`),
+  CONSTRAINT `moviecol_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
+  CONSTRAINT `moviecol_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `moviecol`
+--
+
+LOCK TABLES `moviecol` WRITE;
+/*!40000 ALTER TABLE `moviecol` DISABLE KEYS */;
+INSERT INTO `moviecol` VALUES (3,1,3,'2017-10-15 18:06:50'),(4,1,1,'2017-10-15 18:06:51'),(5,1,8,'2017-10-18 22:56:44');
+/*!40000 ALTER TABLE `moviecol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oplog`
+--
+
+DROP TABLE IF EXISTS `oplog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oplog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_id` int(11) DEFAULT NULL,
+  `ip` varchar(64) DEFAULT NULL,
+  `reason` varchar(512) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `admin_id` (`admin_id`),
+  KEY `ix_oplog_addtime` (`addtime`),
+  CONSTRAINT `oplog_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oplog`
+--
+
+LOCK TABLES `oplog` WRITE;
+/*!40000 ALTER TABLE `oplog` DISABLE KEYS */;
+INSERT INTO `oplog` VALUES (1,1,'127.0.0.1','添加标签 abc','2017-10-15 20:30:56'),(2,1,'127.0.0.1','添加标签 def','2017-10-15 20:30:59'),(3,1,'127.0.0.1','添加标签 qwer','2017-10-15 20:31:08');
+/*!40000 ALTER TABLE `oplog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `preview`
+--
+
+DROP TABLE IF EXISTS `preview`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `preview` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) DEFAULT NULL,
+  `logo` varchar(256) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`),
+  UNIQUE KEY `logo` (`logo`),
+  KEY `ix_preview_addtime` (`addtime`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `preview`
+--
+
+LOCK TABLES `preview` WRITE;
+/*!40000 ALTER TABLE `preview` DISABLE KEYS */;
+INSERT INTO `preview` VALUES (1,'梅西点球救主，巴萨带走三分','2017101700030757c62c7ecab84eccbf329d36eb18d12c.jpg','2017-10-17 00:03:08'),(2,'国家德比：第二季','20171017000336c75bbc6af3414b44a223e467233e6eb3.jpg','2017-10-17 00:03:36'),(3,'克鲁伊夫— —拉玛西亚缔造者','20171017000400cf97fcbd572d4b8b978faf0f3b3ec815.jpg','2017-10-17 00:04:01'),(4,'高成绩不胜寒— —球王的孤独','20171017000433c77a4460fa4e46bd93909cdcfa62465c.jpg','2017-10-17 00:04:34'),(5,'小罗征服伯纳乌','201710170005042e976349988e4ffc832cb5cf3d773d71.jpg','2017-10-17 00:05:05');
+/*!40000 ALTER TABLE `preview` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `auths` varchar(512) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `ix_role_addtime` (`addtime`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'superadmin','','2017-10-06 07:35:49'),(3,'标签管理员1','1,2,3,5','2017-10-15 21:49:57');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `ix_tag_addtime` (`addtime`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tag`
+--
+
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+INSERT INTO `tag` VALUES (2,'动作','2017-10-10 22:50:39'),(3,'爱情','2017-10-10 22:51:10'),(7,'科幻','2017-10-11 22:44:04'),(8,'123','2017-10-15 20:27:17'),(9,'abc','2017-10-15 20:30:56'),(10,'def','2017-10-15 20:30:59');
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `pwd` varchar(128) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `phone` varchar(11) DEFAULT NULL,
+  `info` text,
+  `face` varchar(255) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  `uuid` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `phone` (`phone`),
+  UNIQUE KEY `face` (`face`),
+  UNIQUE KEY `uuid` (`uuid`),
+  KEY `ix_user_addtime` (`addtime`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'狗','1241','1241@123.com','13888888888','狗',NULL,'2017-10-15 16:38:44','hypoiuahgu3hiuh32'),(3,'羊','1231','1231@123.com','13999999999','羊','1f411.png','2017-10-15 16:39:56','hypoiuahlouihk35y'),(8,'abc','pbkdf2:sha256:50000$bvbFGkln$06a0836e6f72eece2694732a33c4ad374eb4cd5cc0e5cf74a1c1131726c91b38','campnou_liu@163.com','18696162095','awrtq345','2017101623040881f4a9b83c60468c9720a0539b4c394f.jpg','2017-10-16 21:24:41','8d83d881ef3548b9af57019572c6b573'),(9,'test','pbkdf2:sha256:50000$Z2pOOONF$b3bffd2db98f3d5862802c1f4330c25025d94d06630a08dd81cd47f2894cd411','7714@qq.com','13987009121',NULL,NULL,'2017-10-16 23:18:04','d0b1913ee8e84d96b923e40633292870');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userlog`
+--
+
+DROP TABLE IF EXISTS `userlog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userlog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `ip` varchar(64) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `ix_userlog_addtime` (`addtime`),
+  CONSTRAINT `userlog_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userlog`
+--
+
+LOCK TABLES `userlog` WRITE;
+/*!40000 ALTER TABLE `userlog` DISABLE KEYS */;
+INSERT INTO `userlog` VALUES (1,1,'192.168.0.1','2017-10-15 20:39:47'),(5,3,'192.168.0.3','2017-10-15 20:41:05'),(6,8,'127.0.0.1','2017-10-16 21:50:40'),(7,8,'127.0.0.1','2017-10-16 21:57:02'),(8,8,'127.0.0.1','2017-10-16 21:58:03'),(9,8,'127.0.0.1','2017-10-16 23:15:04'),(10,8,'127.0.0.1','2017-10-16 23:16:36'),(11,9,'127.0.0.1','2017-10-16 23:18:13'),(12,8,'127.0.0.1','2017-10-16 23:18:49'),(13,8,'127.0.0.1','2017-10-16 23:38:13'),(14,8,'127.0.0.1','2017-10-18 20:31:35'),(15,8,'127.0.0.1','2017-10-18 20:57:30'),(16,8,'127.0.0.1','2017-10-19 20:37:07');
+/*!40000 ALTER TABLE `userlog` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-22 14:10:58
